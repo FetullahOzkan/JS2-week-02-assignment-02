dcc4d5c476d5232757bed4f3b27dd91682889b12
FetullahOzkan <111522168+FetullahOzkan@users.noreply.github.com>
2022-11-24 19:43:10 +0100

    Week02 Assignment02 has done

--- functions.js
+++ functions.js
@@ -1,5 +1,22 @@
-const changeEmailFromGmailToHicoders = () => {};
+const changeEmailFromGmailToHicoders = (pArray) => {
+let result = pArray
+            .filter((number) => number.includes("41"))
+            .map((item) => item.toLowerCase())
+            .sort()
+            .map(item => item.replace("gmail.com", "hicoders.ch"))
+    return  result;
+    
+};
 
-const findSuperNumbers = () => {};
-
+const findSuperNumbers = (pArray) => {
+const result = pArray.filter((item) => {
+  let number1= item %10;
+  let number2= Math.floor(item/10) %10
+  let number3= Math.floor(item/100) %10
+  let total= (Math.pow(number1,3))+ (Math.pow(number2,3))+ (Math.pow(number3,3))
+  return total === item
+} )
+return result
+};
+findSuperNumbers([455]);
 export { changeEmailFromGmailToHicoders, findSuperNumbers };
